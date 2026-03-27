5842586607d0dba2d0175d8012475a89e8447655
jbm950 <jmilam343@gmail.com>
2026-03-27 06:00:15 -0400

    feat(App): move get player to modal screen
    This commit moves the GetPlayer class that loads player data to a
    ModalScreen. It had been set up as a PlotextPlot because I needed it to
    be a widget so that the loaded message could be posted. Getting the data
    though and the plot itself belong as two separate objects.
    
    Also, now there's a nice loading popup with a LoadingIndicator while the
    app reaches out to get data.
    
    Refs:

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from textual.app import App
+from textual.containers import Vertical
 from textual.message import Message
+from textual.screen import ModalScreen
+from textual.widgets import Label, LoadingIndicator
 from textual_plotext import PlotextPlot
 
 from api import get_player
 
 
-class GetPlayer(PlotextPlot):
+class GetPlayer(ModalScreen):
+    def __init__(self, player_name):
+        self.player_name = player_name
+        super().__init__()
+
+    def compose(self):
+        with Vertical():
+            yield Label(f'Loading {self.player_name}', id='loading-label')
+            yield LoadingIndicator()
+
+    def on_mount(self):
+        self.run_worker(self.load(self.player_name))
+
     class Loaded(Message):
         def __init__(self, player):
             self.player = player
@@ -14,25 +29,45 @@ class GetPlayer(PlotextPlot):
     async def load(self, player_name):
         player = await get_player(player_name)
         self.post_message(self.Loaded(player))
+        self.app.pop_screen()
 
 
 class RunescapeApp(App):
     CSS = """
-    GetPlayer {
+    PlotextPlot {
         padding: 4 0 0 0
+    }
+
+    GetPlayer {
+        align: center middle;
+    }
+
+    Vertical {
+        height: 10;
+        width: 40;
+        border: thick $background 80%;
+        background: $surface;
+    }
+
+    #loading-label {
+        content-align: center middle;
+        width: 1fr;
+        height: 3;
+    }
+
+    LoadingIndicator {
+        padding: 0 0 3 0;
     }
     """
     def compose(self):
-        yield GetPlayer()
+        yield PlotextPlot()
 
     def on_mount(self):
-        self.run_worker(self.query_one(PlotextPlot).load('Salvsis2'))
+        self.push_screen(GetPlayer('Salvsis2'))
 
     def on_get_player_loaded(self, message):
-        self.notify('received player loaded message')
-
         skills = message.player.skills
-        plot_widget = self.query_one(GetPlayer)
+        plot_widget = self.query_one(PlotextPlot)
         plt = plot_widget.plt
         plt.clear_data()
         plt.clear_figure()
